353d9e77dacfb520538a3c832ce579a9db453ad2
Sean Delaney <seanmartindelaney@gmail.com>
2020-09-18 20:30:22 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,23 @@
 def new_hash
-  # return an empty hash
+  basketball = {
+  }
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  baseball = {
+    cleveland: "Indians"
+  }
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneers = {
+    name: "Grace Hopper"
+  }
 end
 
 
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+  number_hash = {
+    id: number
+  }
 end
